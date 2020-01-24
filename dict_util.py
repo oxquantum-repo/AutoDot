872ac6c058e0237d34b72b07e04e140c85334b24
Dominic <lennondom@gmail.com>
2020-01-24 16:54:01 +0000

    restructured sampler

---
+++ dict_util.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 24 16:24:19 2020
+
+@author: Dominic
+"""
+import pickle
+import warnings
+
+class Tuning_dict(dict):
+    def __init__(self,*args,**kwargs):
+        super(Tuning_dict,self).__init__(*args,**kwargs)
+        
+    def get(self,*var):
+        return tuple(self[k] for k in var)
+    
+    def getd(self,*var):
+        return dict(tuple((k,self[k]) for k in var))
+    
+    def getl(self,*var):
+        cand = [self[k] for k in var]
+        return tuple(c[-1] if isinstance(c,list) else c for c in cand)
+ 
+    
+    def app(self,**kwargs):
+        dict_return = {}
+        for key,item in kwargs.items():
+            try:
+                self[key].append(item)
+                dict_return[key] = self[key]
+            except AttributeError:
+                raise warnings.warn("%s is not a list")
+        return dict_return
+    
+    def add(self,**kwargs):
+        for key,item in kwargs.items():
+            self[key] = item
+            
+    def save(self,file_pth=None):
+        if file_pth is None:
+            file_pth = self['save_dir']+self['file_name']
+        with open(file_pth,'wb') as h:
+            pickle.dump(self,h)
