371ecc1069623e44f7ddae0f49beeaa1fedf7f92
brandonseverin <brandon.severin@materials.ox.ac.uk>
2020-03-04 11:11:41 +0000

    added timer to utils.py

--- main_utils/utils.py
+++ main_utils/utils.py
@@ -1,7 +1,69 @@
+# 20200304
+# Brandon Severin
+
+# Brief Info 
+# Logs and stores the times of steps during the tuning algorithm
 
 
+# Import modules
+import time
 
+# Define Timer Error class
+class TimerError(Exception): 
+    """ A custom exception used to report errors in use of Timer class"""
+        
+# Define Timer class
+class Timer():
+    
+    def __init__(self, elapsed_time_text="Elapsed time: {:0.4f} seconds", runtime_text="Current runtime: {:0.4f} seconds"):
+        # Initialise <._start_time> attrtibute
+        self._start_time = None
+        # create format: default text to report current runtime for logging and elapsed time upon stopping timer
+        self.runtime_text = runtime_text
+        self.elapsed_time_text = elapsed_time_text
+        # initiate times list
+        self.times_list = []
 
+    # start the timer
+    def start(self):
+        # Check if the timer is already running
+        if self._start_time is not None:
+            raise TimerError(f"Timer is running, Use .stop() to stop it")
+        # start timer
+        self._start_time = time.perf_counter()
+        # Append an empty list (for storing logtimes) to times list
+        self.times_list.append([])
+    
+    # Log time elapsed, keep timer running
+    def logtime(self):
+        # First, check if the timer is running
+        if self._start_time is None:
+            raise TimerError(f"Timer is not running. Use .start() to start it.")
 
+        # calculate current runtime
+        runtime = time.perf_counter() - self._start_time
+        # Append runtime to list within times_list
+        self.times_list[-1].append(runtime)
 
-pass
+        # print current runtime to console by first formatting text template
+        print(self.runtime_text.format(runtime))
+    
+    # stop the timer
+    def stop(self):
+        # stop timer, and report the elapsed time
+        # First, check if the timer is running
+        if self._start_time is None:
+            raise TimerError(f"Timer is not running. Use .start() to start it.")
+
+        # calculate total elapsed time 
+        elapsed_time = time.perf_counter() - self._start_time
+        
+        # Append elapsed time to list within times_list
+        self.times_list[-1].append(elapsed_time)
+        
+        # reset start time back to 'None' so that the timer can be restarted
+        self._start_time = None
+       
+        # print elapsed time to console by first formatting text template
+        print(self.elapsed_time_text.format(elapsed_time))
+
