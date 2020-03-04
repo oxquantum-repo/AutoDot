3740b29c9cec7052fd1d95216c476ceee83958b9
Dominic <lennondom@gmail.com>
2020-03-04 11:54:57 +0000

    changed to relitive imports

--- Investigation/Investigation_factory.py
+++ Investigation/Investigation_factory.py
@@ -4,8 +4,8 @@ Created on Thu Nov 14 10:28:23 2019
 
 @author: thele
 """
-import .measurement_functions as measurement_functions
-import .condition_functions as condition_functions
+from . import measurement_functions
+from . import condition_functions
 import numpy as np
 
 
