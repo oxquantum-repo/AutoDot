0f3065b5f602fddeb49238e711df1ab067941cc8
Dominic Lennon <lennondom@gmail.com>
2020-04-10 11:58:27 +0100

    fixed enviroment error

--- Playground/mock_device.py
+++ Playground/mock_device.py
@@ -9,8 +9,9 @@ def build_mock_device_with_json(config):
     for key,item in config['shapes'].items():
         shapes_list += [getattr(shapes,key)(config['ndim'], **item)]
         
-        
-    device = Device(shapes_list,config['ndim'])
+    dirs = config.get('dir',None)
+    origin = config.get('origin',None)
+    device = Device(shapes_list,config['ndim'],origin,dirs)
         
         
     return device
@@ -22,7 +23,7 @@ class scale_for_device():
     def __init__(self,origin,dir):
         self.origin = origin
         self.dir = dir
-    def __call__(self,params,bc=False):
+    def __call__(self,params,bc=True):
         if bc:
             return  (params - self.origin[np.newaxis,:])*self.dir[np.newaxis,:]
         else:
@@ -31,8 +32,8 @@ class scale_for_device():
 class Device():
     def __init__(self,shapes_list,ndim,origin=None,dir=None):
     
-        origin = np.array([0]*ndim) if origin is None else origin
-        dir = np.array([-1]*ndim) if dir is None else dir
+        origin = np.array([0]*ndim) if origin is None else np.array(origin)
+        dir = np.array([-1]*ndim) if dir is None else np.array(dir)
         
         #Negative dir prefered so flip signs
         dir = dir*-1
@@ -51,7 +52,7 @@ class Device():
         return self.params.squeeze() if idx is None else self.params.squeeze()[idx]
     
     def arr_measure(self,params):
-        shape_logical = [shape(params) for shape in self.shapes_list]
+        shape_logical = [shape(self.sd(params)) for shape in self.shapes_list]
         
         shape_logical = np.array(shape_logical)
         
