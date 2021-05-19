1d06afb68b4e2429aa23c5ac667dfcaee8b5d12f
DuckNrOne <tony.kamp@web.de>
2021-05-19 10:24:39 +0200

    Add more options to score functions

--- Investigation/score_functions.py
+++ Investigation/score_functions.py
@@ -6,6 +6,7 @@ Created on Thu Mar 18 08:30:00 2021
 """
 import numpy as np
 
+
 def score_nothing(invest_em_results, config):
     return 0
 
@@ -18,4 +19,4 @@ def mock_stage_score(invest_results, config):
     if invest_results['conditional_idx'] < config.get("stage"):
         return np.inf
     else:
-        return -invest_results['extra_measure'][config.get("stage") - 1][0]
+        return config.get("coefficient", -1)*invest_results['extra_measure'][config.get("stage") - 1][0]
