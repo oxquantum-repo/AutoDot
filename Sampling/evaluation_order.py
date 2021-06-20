56239a43ab0ed98d489f5748662a3937f2078ca9
DuckNrOne <tony.kamp@web.de>
2021-06-20 19:59:37 +0200

    add 0 check to angel, fix spanning tree, minimum match, order from eulerian, clear eulerian tour

--- Sampling/evaluation_order.py
+++ Sampling/evaluation_order.py
@@ -1,14 +1,15 @@
 import numpy as np
-from heapq import heappush, heappop
+from heapq import heapify, heappush, heappop
 import random
 import math
 
 def get_evaluation_order(population, curr_pos):
 
+    # christofides algorithm as approximation of TSP
     adj_matrix = build_graph(population, curr_pos)
     MSTree = minimum_spanning_tree(adj_matrix)
     add_minimum_weight_matching(MSTree, adj_matrix)
-    eulerian_tour = get_eulerian_tour(MSTree, adj_matrix)
+    eulerian_tour = get_eulerian_tour(MSTree)
     evaluation_order = get_order_from_euler(eulerian_tour)
 
     return evaluation_order
@@ -19,9 +20,10 @@ def build_graph(population, start_vec):
     for vA in population:
         for vB in population:
             unshaped_graph.append(get_angel(vA, vB))
-        # add start vertex 
+        # add column of current vector
         unshaped_graph.append(get_angel(vA, start_vec))
 
+    # add start row of current vector
     unshaped_graph += [get_angel(v, start_vec) for v in population]
     unshaped_graph.append(0)
 
@@ -37,68 +39,78 @@ def get_angel(vA, vB):
     def length(v):
         return math.sqrt(dotproduct(v, v))
 
-    return math.acos(dotproduct(vA, vB) / (length(vA) * length(vB)))
+    if np.array_equiv(vA, vB):
+        return 0
+
+    return abs(math.acos(dotproduct(vA, vB) / (length(vA) * length(vB))))
 
 
 def minimum_spanning_tree(graph):
-    adj_list = len(graph)*[[]]
-    pq = []
-    distances = np.array(len(graph)*[np.inf])
-    heappush(pq, (0, 0, 0))
+    start_vert = 0
+    pq = [
+        (length, start_vert, next_vert)
+        for next_vert, length in enumerate(graph[start_vert])
+        if next_vert != start_vert
+    ]
+    heapify(pq)
+    adj_list = [[] for _ in range(len(graph))]
+    visited = set([start_vert])
 
     while len(pq) != 0:
-        last_distance, curr_vec, last_point = heappop(pq)
-
-        # ignore already connected points
-        if last_distance > distances[curr_vec]:
+        _, last_vert, next_vert = heappop(pq)
+        if next_vert in visited:
             continue
+        visited.add(next_vert)
+        adj_list[next_vert].append(last_vert)
+        adj_list[last_vert].append(next_vert)
 
-        # skip first iteration
-        if(curr_vec):
-            adj_list[curr_vec].append(last_point)
-            adj_list[last_point].append(curr_vec)
-        
-        for another_point, distance in enumerate(graph[curr_vec]):
-            if another_point == curr_vec:
+        for another_point, distance in enumerate(graph[next_vert]):
+            if another_point == next_vert:
                 continue
-            if distances[another_point] > distance:
-                distances[another_point] = distance
-                heappush(pq, (distance, another_point, curr_vec))
-    
+            if another_point not in visited:
+                heappush(pq, (distance, next_vert, another_point))
     return adj_list
 
 
 def add_minimum_weight_matching(MST, G):
     odd_vert = [vec for vec in range(len(MST)) if len(MST[vec]) % 2 == 1]
-    random.shuffle(odd_vert)
+    num_odd_vert = len(odd_vert)
 
+    # TODO use blossom v algorithm instead of greedy
+
+    # generate list of edges between vertices with odd degree
+    edges = []
     while len(odd_vert) != 0:
         v = odd_vert.pop()
-        length = np.inf
-        closest = 0
         for u in odd_vert:
-            if G[v][u] < length:
-                length = G[v][u]
-                closest = u
+            edges.append((G[u][v],u,v))
 
-        MST[v].append(closest)
-        MST[closest].append(v)
-        odd_vert.remove(closest)
+    # add smallest edges between vertices with odd degree to spanning tree
+    edges.sort()
+    completed_vert = set()
+    while len(completed_vert) != num_odd_vert:
+        _, u, v = edges.pop()
+        if u not in completed_vert and v not in completed_vert:
+            completed_vert.add(u)
+            completed_vert.add(v)
+            MST[u].append(v)
+            MST[v].append(u)
 
         
 def get_eulerian_tour(MatchedMSTree):
 
-    path = []
     def visit_next(v):
-        while MatchedMSTree[v] != 0:
+        path = []
+        while len(MatchedMSTree[v]) != 0:
             w = MatchedMSTree[v][0]
-            del MatchedMSTree[v][MatchedMSTree.index(w)]
-            del MatchedMSTree[w][MatchedMSTree.index(v)]
-            visit_next(w)
+            MatchedMSTree[v].remove(w)
+            MatchedMSTree[w].remove(v)
+            path.extend(visit_next(w))
         path.append(v)
+        return path
     
-    # Start at current point
-    visit_next(len(MatchedMSTree)-1)
+    # start eulerian tour at current vector
+    path = visit_next(len(MatchedMSTree)-1)
     path.reverse()
     return path
 
@@ -107,9 +119,9 @@ def get_order_from_euler(eulerian_tour):
     path = []
     # ignore current point point in path
     visited = set([eulerian_tour[0]])
-
+    
     for v in eulerian_tour:
-        if not visited[v]:
+        if v not in visited:
             path.append(v)
             visited.add(v)
     return path
